84992459c01dc07b96b8ecff0714838544ff02ed
Ray <pranshuchawla19@gmail.com>
2024-08-17 16:11:21 -0400

    created objs of personal projects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
+import { populateProjectList } from './populateProjectList';
 
-const populateProjectList = require('./populateProjectList');
 
 populateProjectList();
--- src/populateProjectList.js
+++ src/populateProjectList.js
@@ -8,9 +8,21 @@ function populateProjectList() {
     }
 
     const projects = [
-        createProject('Collect Computer Info Script', 'This script collects computer information using the STAR method.', 'https://example.com/collectComputerInfo'),
-        createProject('Calculator', 'A simple calculator application built with JavaScript.', 'https://example.com/calculator'),
-        createProject('Rock Paper Scissors', 'A Rock Paper Scissors game implemented in JavaScript.', 'https://example.com/rockPaperScissors')
+        createProject(
+            'Collect Computer Info Script',
+            "<p><b>Features:</b> Retrieves and records the computer's serial number, hostname, model, Ethernet MAC address, and Wi-Fi MAC address; Outputs the collected information to a text file on the USB drive; Automatically appends new information to the file if it already exists; Handles errors gracefully and provides user feedback.</p><p><b>Technologies</b>: PowerShell, Bash. <p><p><b>Outcome:</b> Enabled efficient and automated collection of critical system information, simplifying inventory and troubleshooting processes and reducing time spent on inventory by 88%.</p>",
+            'https://example.com/collectComputerInfo'
+                    ),
+        createProject(
+            'Standard Calculator',
+            "<p><b>Features:</b> Basic arithmetic operations: addition, subtraction, multiplication, and division, user-friendly interface, a clear button for easy reset of the calculation, and error handling for invalid inputs.</p><p><b>Technologies:</b> Javascript, Bootstrap Framework.</p><p><b>Outcome:</b> Provided users with an efficient and intuitive tool for performing everyday calculations, enhancing user experience through a clean and simple interface.</p>",
+            'https://rayfrightener.github.io/calculator/'
+        ),
+        createProject(
+            'Rock, Paper, Scissors Game',
+            "<b>Features:</b> Implements a simple interactive game where the player competes against the computer; Tracks and displays the player's and computer's scores in real-time; Provides immediate feedback on each round's result (win, lose, or draw); Ends the game when either the player or the computer reaches 5 points; Offers a restart button to begin a new game session.<p><b>Technologies:</b> JavaScript, HTML, CSS.</p><p><b>Outcome:</b> Developed a functional and engaging web application that enhances understanding of JavaScript event handling, DOM manipulation, and game logic. Improved programming skills and demonstrated the ability to create interactive user experiences.</p>",
+            'https://rayfrightener.github.io/rock-paper-scissors/'
+        )
     ];
 
     const projectList = document.getElementById('projectList');
@@ -28,7 +40,7 @@ function populateProjectList() {
         summary.textContent = project.name;
 
         const description = document.createElement('p');
-        description.textContent = project.description;
+        description.innerHTML = project.description;
 
         const link = document.createElement('a');
         link.href = project.link;
